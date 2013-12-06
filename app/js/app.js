22cde33958ea4e855ba22608e48982c659a0f2c3
Roman Krejcik <farin@farin.cz>
2013-12-06 16:51:36 +0100

    routing

--- app/js/app.js
+++ app/js/app.js
@@ -2,10 +2,37 @@ $.ajaxSetup({
     contentType: 'application/json'
 });
 
-var app = angular.module('growduino', ['ngResource']);
+var app = angular.module('growduino', ['ngResource', 'ngRoute']);
 
 app.factory('NetworkConfig', ['$resource', function($resource) {
     return $resource('/config.jso');
+}]);
+
+
+app.config(['$routeProvider', function($routeProvider) {
+    $routeProvider.
+        when('/', {
+            templateUrl: 'partials/home.html',
+            controller: 'HomeCtrl'
+        }).
+        when('/network', {
+            templateUrl: 'partials/network.html',
+            controller: 'NetworkConfigCtrl'
+        }).
+        otherwise({
+           redirectTo: '/'
+        });
+}]);
+
+
+app.controller('NavCtrl', ['$scope', '$location', function($scope, $location) {
+    $scope.isActive = function(loc) {
+        return loc === $location.path();
+    };
+}]);
+
+app.controller('HomeCtrl', ['$scope', function($scope) {
+    //TODO
 }]);
 
 app.controller('NetworkConfigCtrl', ['$scope', 'NetworkConfig', function($scope, NetworkConfig) {
