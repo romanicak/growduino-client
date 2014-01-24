af781df8ef5edb7ccda2909789dbcded1032394b
Roman Krejcik <farin@farin.cz>
2014-01-24 21:49:06 +0100

    serial update

--- app/js/controllers/ChartController.js
+++ app/js/controllers/ChartController.js
@@ -5,6 +5,9 @@ app.controller('ChartController', ['$scope', 'Temperature', 'Humidity', 'Lightin
         {name: 'Humidity', resource: Humidity, yAxis: 1},
         {name: 'Lighting', resource: Lighting, yAxis: 1},
     ];
+    var q = async.queue(function(task, done) {
+        task(done);
+    }, 1);
 
     function initChart() {
         chart = new Highcharts.Chart({
@@ -31,54 +34,68 @@ app.controller('ChartController', ['$scope', 'Temperature', 'Humidity', 'Lightin
             ],
             series: []
         });
+        series.forEach(function(s) {
+            chart.addSeries({
+                name: s.name,
+                yAxis: s.yAxis
+            });
+        });
+        cleanChart();
     }
 
     function cleanChart() {
-        //TODO use series.setData insead !!!
-        while (chart.series.length > 0) {
-            chart.series[0].remove(chart.series.length == 1);
-        }
+        chart.series.forEach(function(s) {
+            s.hide();
+        });
+        q.tasks.splice(0, q.tasks.length);
+        //chart.showLoading();
     }
 
     function showRecent() {
-        //$scope.loading = true;
         cleanChart();
         var pointStart =  (new Date()).getTime() - 60*1000;
-        series.forEach(function(item) {
-            item.resource.get(function(data) {
-                $scope.loading = false;
-                chart.addSeries({
-                    name: item.name,
-                    data: data.min,
-                    yAxis: item.yAxis,
-                    pointStart: pointStart,
-                    pointInterval: 1000 * 60 //1 min
+        series.forEach(function(item, i) {
+            chart.series[i].update({
+                data: [],
+                pointStart: pointStart,
+                pointInterval: 1000 * 60 //1 min
+            });
+            q.push(function(done) {
+                item.resource.get(function (data) {
+                    //chart.hideLoading();
+                    chart.series[i].setData(data.min);
+                    chart.series[i].show();
+                }).$promise.finally(function() {
+                    chart.series[i].show();
+                    done();
                 });
             });
         });
     }
 
     function showMonth(dt) {
-        //$scope.loading = true;
         cleanChart();
         var args = {
             year: utils.formatDate(dt, 'yyyy'),
             month: utils.formatDate(dt, 'mm')
         };
-        series.forEach(function(item) {
-            item.resource.getMonth(args, function(data) {
-                $scope.loading = false;
-                chart.addSeries({
-                    name: item.name,
-                    data: data.day,
-                    yAxis: item.yAxis,
-                    pointStart: dt.getTime(),
-                    pointInterval: 1000 * 60 * 60 * 24 //day
+        series.forEach(function(item, i) {
+            chart.series[i].update({
+                data: [],
+                pointStart: dt.getTime(),
+                pointInterval: 1000 * 60 * 60 * 24 //day
+            });
+            q.push(function(done) {
+                item.resource.getMonth(args, function(data) {
+                    //chart.hideLoading();
+                    chart.series[i].setData(data.day);
+                }).$promise.finally(function() {
+                    chart.series[i].show();
+                    done();
                 });
             });
         });
     }
-
 
     $('#picker-month').datetimepicker({
         minView: 3,
@@ -86,7 +103,7 @@ app.controller('ChartController', ['$scope', 'Temperature', 'Humidity', 'Lightin
         startView: 3,
         format: 'MM yyyy',
         language: 'cs',
-        //startDate: new Date()
+        startDate: new Date(2013, 0, 1),
         endDate: new Date()
     }).on('changeDate', function(ev){
         showMonth(ev.date);
