c1a6c0fdc63533165c0e905032361634eb8b0913
Roman Krejcik <farin@farin.cz>
2014-01-26 12:43:26 +0100

    Grunt build

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,87 +1,88 @@
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
+// Project configuration.
+grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     watch: {
-      /*css: {
-        files: [
-          '** /*.scss'
-        ],
-        tasks: ['sass']
-      },*/
-      js: {
-        files: [
-          'src/js/**/*.js',
-          'Gruntfile.js'
-        ],
-        tasks: ['jshint']
-      }
+        /*css: {
+            files: [
+              '** /*.scss'
+            ],
+            tasks: ['sass']
+        },*/
+        js: {
+            files: [
+                'src/js/**/*.js',
+                'Gruntfile.js'
+            ],
+            tasks: ['jshint']
+        }
     },
     jshint: {
-      options: {
-        jshintrc: '.jshintrc'
-      },
-      all: ['Gruntfile.js', 'src/js/**/*.js']
+        options: {
+            jshintrc: '.jshintrc'
+        },
+        all: ['Gruntfile.js', 'src/js/**/*.js']
     },
     clean: {
-      dist: {
-        src: [ 'dist' ]
-      },
+        dist: {
+            src: [ 'dist' ]
+        },
+        tmp: {
+            src: ['.tmp']
+        }
     },
     copy: {
-      dist: {
-        cwd: 'src',
-        src: [ 'images/*', 'partials/*', '*.htm' ],
-        dest: 'dist',
-        expand: true
-      },
+        dist: {
+            cwd: 'src',
+            src: [ 'images/*', 'partials/*'],
+            dest: 'dist',
+            expand: true
+        },
+        htmltmp: {
+            cwd: 'src',
+            src: [ 'index.htm' ],
+            dest: '.tmp',
+            expand: true
+        }
     },
     htmlmin: {
-      dist: {
+        dist: {
+            options: {
+                removeComments: true,
+                collapseWhitespace: true
+            },
+            files: {
+                'dist/index.htm': '.tmp/index.htm'
+            }
+        }
+    },
+    useminPrepare: {
+        html: '.tmp/index.htm',
         options: {
-          removeComments: true,
-          collapseWhitespace: true
+            root: 'src'
         }
-      },
-      files: {
-        'dist/index.htm': 'src/index.htm'
-      }
     },
-    cssmin: {
-      dist: {
-        files: {
-          'dist/minified.css': [ 'src/libs/**/*.css', 'src/css/**/*.css' ]
-        }
-      }
-    },
-    uglify: {
-      dist: {
-        files: {
-          'dist/minified.js': [
-            'src/libs/bootstrap-datetimepicker/bootstrap-datetimepicker.min.js',
-            'src/libs/bootstrap-datetimepicker/bootstrap-datetimepicker.cs.js',
-            'src/libs.async.js',
-            'src/js/utils.js',
-            'src/js/app.js',
-            'src/js/controllers/**/*.js'
-          ]
-        }
-      }
+    usemin: {
+        html: '.tmp/index.htm'
     }
-  });
+});
 
-  // Load the Grunt plugins.
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
+// Load the Grunt plugins.
+grunt.loadNpmTasks('grunt-contrib-watch');
+grunt.loadNpmTasks('grunt-contrib-jshint');
 
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-htmlmin');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+grunt.loadNpmTasks('grunt-contrib-clean');
+grunt.loadNpmTasks('grunt-contrib-copy');
+grunt.loadNpmTasks('grunt-contrib-htmlmin');
+grunt.loadNpmTasks('grunt-contrib-concat');
+grunt.loadNpmTasks('grunt-contrib-cssmin');
+grunt.loadNpmTasks('grunt-contrib-uglify');
+grunt.loadNpmTasks('grunt-usemin');
 
-  // Register the default tasks.
-  grunt.registerTask('default', ['watch']);
-  grunt.registerTask('dist', ['clean', 'copy', 'htmlmin', 'cssmin', 'uglify']);
+// Register the default tasks.
+grunt.registerTask('default', ['watch']);
+//grunt.registerTask('dist', ['clean', 'copy', 'htmlmin', 'cssmin', 'uglify']);
+grunt.registerTask('dist', ['clean:dist', 'copy', 'useminPrepare','concat', 'cssmin', 'uglify', 'usemin', 'htmlmin', 'clean:tmp']);
+
 };
