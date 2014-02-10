37647548a4c03c41d34f4aecfa3666b0f4a27938
Roman Krejcik <farin@farin.cz>
2014-02-10 22:59:19 +0100

    trigger load

--- src/js/controllers/TriggersController.js
+++ src/js/controllers/TriggersController.js
@@ -11,15 +11,23 @@ app.controller('TriggersController', ['$scope', '$http', 'MAX_TRIGGER', function
 
     $scope.timers = [[], []];
 
-    $scope.rangeSentinels = [
-        {name: 'temperature', unit: '°C', from: null, to: null, active: false, sensor: 0}, //temp
-        {name: 'humidity', unit: "%", from: null, to: null, active: false, sensor: 1} //humidity
-    ];
-    var fan = $scope.fan = {after: null, duration: null, active: false};
-
     var DISABLED_TRIGGER = {t_since:-1, t_until:-1, on_value:-256, off_value:-512, sensor:-1, output:-1};
     var FAN_OUTPUT = 3;
     var RANGE_TRIGGERS = 4; //system triggers
+
+    var SENSOR_TEMP = 0,
+        SENSOR_HUMIDITY = 1;
+
+
+    $scope.rangeSentinels = [
+        {name: 'temperature', unit: '°C', from: null, to: null, active: false, sensor: SENSOR_TEMP}, //temp
+        {name: 'humidity', unit: "%", from: null, to: null, active: false, sensor: SENSOR_HUMIDITY} //humidity
+    ];
+    var fan = $scope.fan = {after: null, duration: null, active: false};
+
+
+
+    $scope.loading = true;
 
     function createRangeTrigger(sensor, output, on, off) {
         return {t_since: -1, t_until:0, on_value: ">"+on, off_value:"<"+off, sensor: sensor, output: output};
@@ -81,16 +89,50 @@ app.controller('TriggersController', ['$scope', '$http', 'MAX_TRIGGER', function
         sentinel.active = !sentinel.active;
     };
 
+    function handleTriggerLoad(t) {
+        for (var i = 0; i< $scope.rangeSentinels.length; i++) {
+            var range = $scope.rangeSentinels[i];
+            if (range.sensor == t.sensor && (t.t_since == -1 && t.t_until === 0)) {
+                if (t.off_value.indexOf('!') === -1) {
+                    range.from = parseInt(t.on_value.substring(1), 10) / 10;
+                    range.to = parseInt(t.off_value.substring(1), 10) / 10;
+                    range.active = true;
+                }
+            }
+        }
+        if (t.output == FAN_OUTPUT && (t.t_since == -1 && t.t_until === 0) && t.on_value[0] === 'T' && t.off_value[0] === 'T') {
+            fan.after = parseInt(t.on_value.substring(1), 10);
+            fan.duration = parseInt(t.off_value.substring(1), 10);
+            fan.active = true;
+        }
+    }
+
+    function loadTriggers() {
+        var triggers = [];
+        var q = async.queue(function(index, done) {
+            $http.get('/triggers/'+index+'.jso').success(function(data) {
+                console.log('Loaded trigger #'+index, data);
+                handleTriggerLoad(data);
+            }).finally(done);
+        }, 1);
+        q.drain = function() {
+            $scope.loading = false;
+        };
+
+        for (var i = 0; i < MAX_TRIGGER; i++) {
+            q.push(i);
+        }
+    }
+
     $scope.saveTriggers = function() {
         if ($scope.saving) return;
         $scope.saving = true;
 
         var triggers = serializeTriggers();
-        console.log(triggers);
-
 
         //todo resource service?
         var q = async.queue(function(trigger, done) {
+            console.log('Savint trigger #'+trigger.index, trigger.data);
             $http.post('/triggers/'+trigger.index+'.jso', trigger.data).finally(done);
         }, 1);
         q.drain = function() {
@@ -105,6 +147,7 @@ app.controller('TriggersController', ['$scope', '$http', 'MAX_TRIGGER', function
         }
     };
 
+    loadTriggers();
 }]);
 
 app.controller('TimerController', ['$scope', function($scope) {
