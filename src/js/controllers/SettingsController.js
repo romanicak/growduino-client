d1e1ec5e134f273ed8c1bbd264523231db38ad2e
Jan Onderek <jan.onderek@bileto.com>
2018-12-06 08:10:10 +0100

    Add basics of connecting to wifi.

--- src/js/controllers/SettingsController.js
+++ src/js/controllers/SettingsController.js
@@ -6,7 +6,10 @@ app.controller('SettingsController', ['$scope', '$timeout', 'BackendConfig', fun
         config.use_dhcp = !!config.use_dhcp; //convert to boolean - TODO move on resource layer
         $scope.config = config;
         $scope.loading = false;
-
+        $scope.known_wifis = [];
+        $scope.selected_index_in_modal = -1;
+        $scope.model={};
+        $scope.model.entered_password = "";
     });
 
     $scope.save = function() {
@@ -22,5 +25,98 @@ app.controller('SettingsController', ['$scope', '$timeout', 'BackendConfig', fun
         }, function() {
             alert('Oops save failed.');
         });
+    };
+
+    $scope.scan_for_wifis = function() {
+        $scope.show_wifi_window = true;
+        known_wifis = [ { 
+            quality: "88%",
+            ssid: "ASBNJOS",
+            frequency: "2,457",
+            channel: "42",
+            encrypted: false
+        }, {
+            quality: "88%",
+            ssid: "ASBNJOS",
+            frequency: "2,457",
+            channel: "42",
+            encrypted: false
+        }, {
+            quality: "88%",
+            ssid: "ASBNJOS",
+            frequency: "2,457",
+            channel: "42",
+            encrypted: true
+        }, {
+            quality: "88%",
+            ssid: "ASBNJOS",
+            frequency: "2,457",
+            channel: "42",
+            encrypted: false
+        }, {
+            quality: "88%",
+            ssid: "jsdal",
+            frequency: "2,457",
+            channel: "42",
+            encrypted: true
+        }, {
+            quality: "88%",
+            ssid: "ASBNJOS",
+            frequency: "2,457",
+            channel: "42",
+            encrypted: false
+        }, {
+            quality: "88%",
+            ssid: "ASBNJOS",
+            frequency: "2,457",
+            channel: "42",
+            encrypted: false
+        }, {
+            quality: "88%",
+            ssid: "dasasdaj",
+            frequency: "2,457",
+            channel: "42",
+            encrypted: true
+        }, {
+            quality: "88%",
+            ssid: "ASBNJOS",
+            frequency: "2,457",
+            channel: "42",
+            encrypted: false
+        }, {
+            quality: "88%",
+            ssid: "ASBNJOS",
+            frequency: "2,457",
+            channel: "42",
+            encrypted: false
+        }, {
+            quality: "88%",
+            ssid: "ASBNJOS",
+            frequency: "2,457",
+            channel: "42",
+            encrypted: false
+        } ];
+        angular.copy(known_wifis, $scope.known_wifis);
+    };
+
+    $scope.select_wifi_in_modal = function(wifi_object, wifi_index) {
+        if (! wifi_object.encrypted && $scope.selected_index_in_modal == wifi_index) {
+            $scope.selected_index_in_modal = -1;
+            $scope.select_wifi_in_controller(wifi_object);
+        } else {
+            $scope.model.entered_password = "";
+            $scope.selected_index_in_modal = wifi_index;
+        }
+    };
+
+    $scope.select_wifi_in_controller = function(wifi_object) {
+        //alert($scope.model.entered_password);
+        $scope.config.wifi_ssid = wifi_object.ssid;
+        $scope.config.wifi_pwd = $scope.model.entered_password;
+        $scope.close_wifi_window();
+    };
+
+    $scope.close_wifi_window = function() {
+        $scope.show_wifi_window = false;
     };
 }]);
